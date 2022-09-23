3667f9a3684277f1f0dc14ca2e86a4ba82e33478
Ihovanna <90429068+Ihovanna@users.noreply.github.com>
2022-09-23 11:02:42 -0700

    merging changes to function_d

--- function_d.py
+++ function_d.py
@@ -2,20 +2,13 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-<<<<<<< HEAD
+
     max_value = 0
     for num in numbers:
         if num > max_value:
             max_value = num
     
     return max_value
-
-=======
-    max_value = max(numbers)
-    return max_value
-
-
->>>>>>> 7791cd9eab1031672918fe23c8221aadd9390f9d
 
 
 if __name__ == "__main__":
