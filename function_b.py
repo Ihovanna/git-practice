9d68b0407fb40295988a9589be07873f1009f4f9
Caitlyn <caitlyn.homet@gmail.com>
2022-09-23 13:42:46 -0400

    formatted to make the red squiggles go away

--- function_b.py
+++ function_b.py
@@ -2,11 +2,13 @@
 # Iterating Over Data
 # Problem-Set While Loops #11
 def silly_sum():
-""" reads numbers from the user
-        summing as we go until either
-        the user enters 0, or
-        the sum reaches or exceeds 1000
+    """ 
+    reads numbers from the user
+            summing as we go until either
+            the user enters 0, or
+            the sum reaches or exceeds 1000
     """
+
     num = int(input('Please enter a number => '))
     sum = 0
 
